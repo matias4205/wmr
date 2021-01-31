639fa83e7c02e3181a305a2b945864d8f056319f
jdecroock <decroockjovi@gmail.com>
2021-01-31 23:42:25 +0100

    try minify

--- src/bundler.js
+++ src/bundler.js
@@ -22,6 +22,7 @@ import optimizeGraphPlugin from './plugins/optimize-graph-plugin.js';
 import externalUrlsPlugin from './plugins/external-urls-plugin.js';
 import copyAssetsPlugin from './plugins/copy-assets-plugin.js';
 import nodeBuiltinsPlugin from './plugins/node-builtins-plugin.js';
+import sucrasePlugin from './plugins/sucrase-plugin.js';
 
 /** @param {string} p */
 const pathToPosix = p => p.split(sep).join(posix.sep);
@@ -94,14 +95,14 @@ export async function bundleProd({
 		plugins: [
 			nodeBuiltinsPlugin({ production: true }),
 			externalUrlsPlugin(),
+			sucrasePlugin({
+				typescript: true,
+				sourcemap,
+				production: true
+			}),
 			swc({
+				minify: true,
 				jsc: {
-					parser: {
-						syntax: 'typescript',
-						jsx: false,
-						tsx: false,
-						dynamicImport: true
-					},
 					target: 'es2017' // can also be ES5
 				}
 			}),
